c2fe247d9678660c97fa4c669afbb0279d1533f6
Sergei Kaukiainen <sergei.kaukiainen@5CG04676KY>
2021-09-23 19:04:02 +0300

    Add hoc that was created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
+import AppContainer from "../../hoc/AppContainer"
+
 const Header = () => {
     return(
-        <div>
-            <h1>Lost in Translation</h1>
-            <h3>
-            <span className="material-icons">catching_pokemon</span>
-            </h3>
-            <hr />
-        </div>
+        <AppContainer>
+            <div>
+                <h1>Lost in Translation</h1>
+                <h3>
+                <span className="material-icons">catching_pokemon</span>
+                </h3>
+                <hr />
+            </div>
+
+        </AppContainer>
     )
 }
 
