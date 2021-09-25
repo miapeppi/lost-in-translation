be3655e4d874692caade0635fb2a420fed3478bc
Sergei Kaukiainen <sergei.kaukiainen@5CG04676KY>
2021-09-25 17:32:09 +0300

    Add updating translations to API

--- src/components/Translation/Translation.jsx
+++ src/components/Translation/Translation.jsx
@@ -1,16 +1,37 @@
 import Input from "./Input";
 import Signs from "./Signs";
 import Header from "../Header/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux"
 import { Container } from "react-bootstrap";
+import { InputAPI } from "./InputAPI";
+import { ProfileAPI } from "../Profile/ProfileAPI";
 
 const Translation = () => {
   const [input, setInput] = useState("");
+  const [state, setState] = useState([])
+
+  useEffect(() => {
+    ProfileAPI.getTranslations(usernameTest)
+      .then(profile => {
+        setState(profile[0])
+      })
+  })
+
+  const { username = '', id } = useSelector(state => state.sessionReducer)
+
+  const idTest = 2
+  const usernameTest = 'serge'
 
   const translateInput = (event, input) => {
     event.preventDefault();
     setInput(input);
-  };
+    const copyWithInput = [...state.translations, input]
+    InputAPI.updateTranslations(idTest, copyWithInput)
+    console.log(username, id);
+  }
+
+
   return (
     <>
       <Header />
@@ -19,7 +40,7 @@ const Translation = () => {
         {input && <Signs userInput={input} />}
       </Container>
     </>
-  );
+  )
 };
 
 export default Translation;
